703db8cf92ba8facc7d69f5181da3a5f6ad43eda
asturiancoder <32171461+asturiancoder@users.noreply.github.com>
2021-10-27 11:32:45 +0200

    Add iOS app (#2)

---
+++ ios/KMM Example/KMM_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KMM_ExampleApp.swift
+//  KMM Example
+//
+//  Created by Felipe Joglar on 27/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct KMM_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
