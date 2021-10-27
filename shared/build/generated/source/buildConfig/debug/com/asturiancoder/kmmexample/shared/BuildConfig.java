a6f70b1e01c6fcc60870d130ff0ea8e174f85e72
asturiancoder <32171461+asturiancoder@users.noreply.github.com>
2021-10-27 22:19:13 +0200

    Add KMM shared module

---
+++ shared/build/generated/source/buildConfig/debug/com/asturiancoder/kmmexample/shared/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.asturiancoder.kmmexample.shared;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.asturiancoder.kmmexample.shared";
+  public static final String BUILD_TYPE = "debug";
+}
